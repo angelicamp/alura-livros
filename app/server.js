003bbd99aed870812967f423f8891d43fe6dc687
angelicamp <enfermagemangelicamp@gmail.com>
2022-09-14 19:40:05 -0300

    1-10 servidor local e Nodemon

--- app/server.js
+++ app/server.js
@@ -1,9 +1,15 @@
 const http = require("http")
 const port = 8080;
 
+const rotas = {
+    '/': 'Bem-vindo ao Alura livros',
+    '/livros': 'Bem-vindo a seleção de livros',
+    '/autores': 'Bem-vindo a seleção de autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'test/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
