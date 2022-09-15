2cb39eb1f13ad4919b68b739285e3238ffde8ede
angelicamp <enfermagemangelicamp@gmail.com>
2022-09-14 21:47:56 -0300

    2-05 Projeto Livraria

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ import app from "./src/app.js";
 
 // process.env.PORT: é a constante que vai identificar a porta que esta sendo escutada
 // ou a porta 8080
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Servidor escutando em http://localhost:'+ port)
--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 
 const app = express();
+app.use(express.json());
+
 const livros = [
     {id: 1, 'Titulo': 'Clean Code'},
     {id: 2, 'Titulo': 'Arquitetura Limpa'}
@@ -12,6 +14,11 @@ app.get('/', (req, res) =>{
 
 app.get('/livros', (req, res) =>{
     res.status(200).json(livros);
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso')
 })
 
 //Importante: é necessario exportar para que o server consiga utilizar esses objetos
